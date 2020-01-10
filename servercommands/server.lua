5c005dbeaf80a9e25cb9ef3d231a5c5cbcd99578
Toxic Developer <59587139+TheRealToxicDev@users.noreply.github.com>
2020-01-10 15:51:51 -0700

    Update and rename servercommands/servercommands/server.lua to servercommands/server.lua

--- servercommands/server.lua
+++ servercommands/server.lua
@@ -49,3 +49,32 @@ function stringsplit(inputstr, sep)
     end
     return t
 end
+
+-- Version Checking down here, better don't touch this
+-- Will throw an error if your version is outdated
+local CurrentVersion = '1.1.0'
+local GithubResourceName = 'Community Commands'
+
+PerformHttpRequest('https://raw.githubusercontent.com/toxic-developer/CommunityCommands/master/' .. GithubResourceName .. '/VERSION', function(Error, NewestVersion, Header)
+	PerformHttpRequest('https://raw.githubusercontent.com/toxic-developer/CommunityCommands/master/' .. GithubResourceName .. '/CHANGES', function(Error, Changes, Header)
+		print('\n')
+		print('##############')
+		print('## ' .. GetCurrentResourceName())
+		print('##')
+		print('## Current Version: ' .. CurrentVersion)
+		print('## Newest Version: ' .. NewestVersion)
+		print('##')
+		if CurrentVersion = NewestVersion then
+			print('## Community Commands Up to date!')
+			print('##############')	
+                 else
+                        print('## Community Commands Outdated')
+			print('## Check the GitHub releases')
+			print('## For the newest Version!')
+                        print('## github.com/TheRealToxicDev/FiveM-Community-Commands/releases')
+			print('##############')
+			print('CHANGES: ' .. Changes)
+                 end
+		print('\n')
+	end)
+end)
